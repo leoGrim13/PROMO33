c9f3896603e09ceab00c34289523e3ddee9fd0b8
Leo Grimaud <lgrimaud.contact@gmail.com>
2023-10-19 00:12:19 +0200

    /

--- src/main/java/com/example/test33/Test33Application.java
+++ src/main/java/com/example/test33/Test33Application.java
@@ -3,11 +3,13 @@ package com.example.test33;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EntityScan("com.example.test33.model")
 @EnableScheduling
+@EnableJpaRepositories
 public class Test33Application {
 
 	public static void main(String[] args) {
